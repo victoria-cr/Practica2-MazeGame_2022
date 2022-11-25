79ab1a0801bd1967469ca1efc0a586a5ba9c0270
victoriacr <victoriacalvrodri@gmail.com>
2022-11-25 12:09:31 +0100

    start

--- src/main/java/com/liceu/practica2/controllers/Start.java
+++ src/main/java/com/liceu/practica2/controllers/Start.java
@@ -8,19 +8,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/inici")
+@WebServlet("/start")
 public class Start extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher =
-                req.getRequestDispatcher("/WEB-INF/jsp/inici.jsp");
+                req.getRequestDispatcher("/WEB-INF/jsp/map1.jsp");
         dispatcher.forward(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher =
-                req.getRequestDispatcher("/WEB-INF/jsp/inici.jsp");
+                req.getRequestDispatcher("/WEB-INF/jsp/map1.jsp");
         dispatcher.forward(req, resp);
     }
 }
