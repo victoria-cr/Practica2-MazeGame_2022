c0d635e1c45965673f2181640c0c504aaa7a29a2
victoriacr <victoriacalvrodri@gmail.com>
2022-11-29 16:20:40 +0100

    remove sout's start

--- src/main/java/com/liceu/practica2/controllers/Start.java
+++ src/main/java/com/liceu/practica2/controllers/Start.java
@@ -1,6 +1,5 @@
 package com.liceu.practica2.controllers;
 
-import com.liceu.practica2.model.Maze;
 import com.liceu.practica2.model.Player;
 import com.liceu.practica2.services.MazeGame;
 import org.json.simple.JSONObject;
@@ -17,6 +16,7 @@ import java.io.IOException;
 @WebServlet("/start")
 public class Start extends HttpServlet {
     MazeGame mazeGame = new MazeGame();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
@@ -30,16 +30,10 @@ public class Start extends HttpServlet {
         HttpSession session = req.getSession();
         Player player = mazeGame.main();
 
-        System.out.println("Player Hecho: " + player);
-
-        System.out.println(player.getCurrentRoom().getNumber());
-
         JSONObject jsonObject = mazeGame.json(player);
-        System.out.println(jsonObject.toJSONString());
         req.setAttribute("json", jsonObject.toJSONString());
 
         session.setAttribute("player", player);
-        System.out.println("Player: " + session.getAttribute("player"));
 
         RequestDispatcher dispatcher =
                 req.getRequestDispatcher("/WEB-INF/jsp/map1.jsp");
